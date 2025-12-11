7f64c636d765dbf0b8486047a243cb8dfdd185fb
Abhishek Asthana <aasthana@sgintl.com>
2025-12-11 16:40:06 +0530

    Image analyzer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ClaimComponent } from './pages/claim/claim.component';
+import { ImageAnalyzerComponent } from './pages/image-analyzer/image-analyzer.component';
 
 export const routes: Routes = [
     {
       path: '',
       component: ClaimComponent,
+    },
+     {
+      path: 'image-analyzer',
+      component: ImageAnalyzerComponent,
     },
 ];
